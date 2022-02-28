21f768347abb0e6762148e2f8cdff0c6aded16d7
DESKTOP-3J65I2E\admin <topdirector2017@gmail.com>
2022-03-01 01:35:18 +0900

    update hardhat config

--- Token-and-Staking-Vault-Contract/hardhat.config.ts
+++ Token-and-Staking-Vault-Contract/hardhat.config.ts
@@ -38,6 +38,11 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    bsctestnet: {
+      url: process.env.BSCTESTNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
